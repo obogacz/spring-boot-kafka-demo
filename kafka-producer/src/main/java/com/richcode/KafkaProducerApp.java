22308eed1ceb67454b1b5bfb662d6afd68a03ec0
Oskar Bogacz <bogacz.oskar3@gmail.com>
2023-11-12 19:20:30 +0100

    feat: Create simple KafkaProducer SpringBoot app

---
+++ kafka-producer/src/main/java/com/richcode/KafkaProducerApp.java
@@ -0,0 +1,12 @@
+package com.richcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApp {
+    public static void main( String[] args ) {
+        SpringApplication.run(KafkaProducerApp.class, args);
+    }
+
+}
